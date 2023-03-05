49915e4c8a1022088edb8c2edc8f953bbcda9193
Vivek Yadav <vivekyadav36837@gmail.com>
2023-03-05 20:22:41 +0530

    Update footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -5,10 +5,10 @@ const Footer = () => {
     <footer className="bg-dark text-light py-3">
       <div className="container">
         <div className="row">
-          <div className="col-md-6">
+          <div className="col-md-4">
             <p>&copy; 2023 Library App. All Rights Reserved.</p>
           </div>
-          <div className="col-md-6">
+          <div className="col-md-4">
             <ul className="list-inline text-md-right">
               <li className="list-inline-item">
                 <a href="/">Privacy Policy</a>
@@ -17,6 +17,9 @@ const Footer = () => {
                 <a href="/">Terms of Use</a>
               </li>
             </ul>
+          </div>
+          <div className="col-md-4">
+            <p>Made by Vivek Yadav</p>
           </div>
         </div>
       </div>
